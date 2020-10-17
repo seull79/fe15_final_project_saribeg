00b1eb8fb3070ed7822cdb93c0456ea061c8080a
Mikhail Scherbina <mikhail.scherbina@gmail.com>
2020-10-18 00:40:45 +0300

    Remove credentinal

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: mongodb+srv://dbUser:uUVKzsDbwUXmrxF3@clusternodejs.ezo4z.gcp.mongodb.net/nodejsdb?retryWrites=true&w=majority
-  secretOrKey: 'REYDFDGWE%#TFGE%^&#HDH$%^&SDFGSWTWTEGSDHWE%Y%$^&WTYHDGFHASREYH$%^&$EYHDFHBDAHW',
+  mongoURI: '',
+  secretOrKey: '',
 };
